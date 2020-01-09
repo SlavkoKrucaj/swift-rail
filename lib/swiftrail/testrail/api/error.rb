0c070c6270b60ff6455aa4d0f30b13dab27797ad
Slavko Krucaj <slavko.krucaj@gmail.com>
2020-01-09 11:11:32 +0100

    Print out the body of the error response

--- lib/swiftrail/testrail/api/error.rb
+++ lib/swiftrail/testrail/api/error.rb
@@ -6,19 +6,19 @@ module Swiftrail
       module Error
         class InvalidRequest < Errors::Base
           def initialize(error_response)
-            super("Invalid or unknown test run/cases #{error_response}")
+            super("Invalid or unknown test run/cases #{error_response.body}")
           end
         end
 
         class NoPermission < Errors::Base
           def initialize(error_response)
-            super("No permissions to add test results or no access to the project #{error_response}")
+            super("No permissions to add test results or no access to the project #{error_response.body}")
           end
         end
 
         class Unknown < Errors::Base
           def initialize(error_response)
-            super("There has been an unknown error #{error_response}")
+            super("There has been an unknown error #{error_response.body}")
           end
         end
       end
